0412ce2240077dd6f4f45f4a33821b3e1ab21bbe
sidg6 <40482115+sidg6@users.noreply.github.com>
2019-08-17 14:36:56 +0530

    CURD operation on employee form

---
+++ main.js
@@ -0,0 +1,169 @@
+
+var requestURL = "http://localhost:8080/Test/json_demo.json"
+var xmlhttp = new XMLHttpRequest();
+
+var readData;
+var modifyData;
+var employeeToDelete;
+
+function readData(pageToLoad){
+	xmlhttp.onreadystatechange = () =>{
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+			var myJsonData = xmlhttp.response;
+			var cacheEmployeeData = myJsonData.Employees;
+			fetchData(cacheEmployeeData, pageToLoad);
+		}
+	};
+		xmlhttp.open("GET",requestURL);
+		xmlhttp.responseType= 'json';
+		xmlhttp.send();
+	}
+
+function fetchData(cacheEmployeeData, pageToLoad){
+	if(cacheEmployeeData!=undefined){
+		var data = cacheEmployeeData;
+		readData = data;
+		modifyData=readData;
+		for(var i=0;i<modifyData.length;i++){
+			if(pageToLoad === "editEmployeePage"){
+				var parent = document.getElementById("employeeDataBody");
+				var parentChild = document.createElement("tr");
+				parent.appendChild(parentChild);
+			}
+			populateData(parentChild, modifyData[i], i, pageToLoad)
+		}
+	}
+}
+
+function populateData(parent, data, i, pageToLoad){
+	if(pageToLoad === "editEmployeePage"){
+		var child = document.createElement("td");
+		var child1 = document.createElement("td");
+		var child2 = document.createElement("td");
+		var child3 = document.createElement("td");
+		var child3Child = document.createElement("i");
+
+		parent.appendChild(child);
+		child.className = "user-id";
+		document.getElementsByClassName("user-id")[i].innerHTML = data.userId;
+		
+		parent.appendChild(child1);
+		child1.className = "title";
+		document.getElementsByClassName("title")[i].innerHTML = data.jobTitleName;
+		
+		parent.appendChild(child2);
+		child2.className = "full-name";
+		document.getElementsByClassName("full-name")[i].innerHTML = data.preferredFullName;
+
+		parent.appendChild(child3);
+		child3.className = "user-edit";
+		child3Child.className = "fa fa-edit";
+		child3.appendChild(child3Child);
+		document.getElementsByClassName("user-edit")[i].addEventListener("click",function(){
+			employeeDetailsPage(data,pageToLoad);
+			document.getElementById("editEmployee").style.display = "block";
+
+		});
+
+		document.getElementsByClassName("user-edit")[i].insertAdjacentHTML("afterend", "<td class=\"delete-user\"><i class='fa fa-trash'></i></td>");
+		document.getElementsByClassName("delete-user")[i].addEventListener("click",function(){
+			myModal(data);
+			document.getElementById("deleteModal").style.display = "block";
+		});
+	}
+}
+
+function sendData(data){
+	xmlhttp.open("POST",requestURL);
+	xmlhttp.responseType= 'json';
+	xmlhttp.send(data);
+	console.log("Create user "+ JSON.stringify(data));
+	localStorage.setItem("create",JSON.stringify(data));
+}
+
+
+
+function employeeDetailsPage(myData,pageToLoad){
+	var data;
+	if(pageToLoad === "editEmployeePage"){
+		data = myData;
+		document.getElementById("deleteModal").disabled = true;
+	}else{
+		var readLocalStorage = localStorage.getItem("create");
+		var parsedData = JSON.parse(readLocalStorage);
+		data = parsedData;
+		localStorage.clear();
+	}
+	document.getElementById("userId").value = data.userId;
+	document.getElementById("jobTitle").value = data.jobTitleName
+	document.getElementById("firstName").value = data.firstName;
+	document.getElementById("lastName").value = data.lastName;
+	document.getElementById("fullName").value = data.preferredFullName;
+	document.getElementById("employeeCode").value = data.employeeCode;
+	document.getElementById("region").value = data.region;
+	document.getElementById("mobileNumber").value = data.phoneNumber;
+	document.getElementById("emailId").value = data.emailAddress;
+}
+
+
+function myModal(data){
+	var employeeToBeDeleted = data.preferredFullName;
+	document.getElementsByClassName("person-deleted")[0].innerHTML = "\""+employeeToBeDeleted+"\"";
+	document.getElementsByClassName("person-deleted")[0].style.color="#aaa222";
+	document.getElementById("editEmployee").style.display = "none";
+	employeeToDelete = data;
+}
+
+window.addEventListener("click", closeModalByClickingOutside);
+
+function closeModal(){
+	document.getElementById("deleteModal").style.display = "none";
+}
+
+function closeModalByClickingOutside(e){
+	if(e.target == document.getElementById("deleteModal")){
+		document.getElementById("deleteModal").style.display = "none";
+	}
+}
+
+function deleteEmployee(){
+	var checkDeleteEmployee = employeeToDelete.employeeCode;
+	var size = readData.length;
+	for(var i=0;i<size;i++){
+		if(checkDeleteEmployee === readData[i].employeeCode){
+			modifyData = readData.filter(function (obj){
+				return obj.employeeCode != readData[i].employeeCode;
+			});
+		}
+	}
+	document.getElementById("employeeDataBody").innerHTML="";
+	fetchData(modifyData, "editEmployeePage");
+	console.log("After Deletion "+ JSON.stringify(modifyData));
+}
+
+
+function editEmployee(data){
+	var updateEmployee = data.employeeCode;
+	var oldModifiedData = modifyData;
+	var size = oldModifiedData.length;
+	for(var i=0;i<size;i++){
+		if(updateEmployee === oldModifiedData[i].employeeCode){
+			modifyData = oldModifiedData.filter(function (obj){
+				return obj.employeeCode != oldModifiedData[i].employeeCode;
+			});
+		}
+	}
+	modifyData.push(data);
+	document.getElementById("employeeDataBody").innerHTML="";
+	fetchData(modifyData, "editEmployeePage");
+	console.log("Edit Employee Details "+JSON.stringify(modifyData));
+}
+
+
+
+
+
+
+
+
+
---
+++ submitForm.js
@@ -0,0 +1,196 @@
+var formData={};
+var submitForm = (pageType) => {
+	if((document.getElementById("userId").value!="")){
+		if((/^[a-z]+$/i).test(document.getElementById("userId").value)){
+			formData.userId = document.getElementById("userId").value;
+			if((document.getElementById("userId").nextSibling.className=="error")){
+				document.getElementsByClassName("error")[0].remove();
+			}
+		}else{
+			var parent = document.getElementById("userId");
+			if((document.getElementById("userId").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the Alphabets only</span>");
+				document.getElementById("userId").focus();
+			}
+		}
+	}else{
+			var parent = document.getElementById("userId");
+			if((document.getElementById("userId").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the user id</span>");
+				document.getElementById("userId").focus();
+			}
+		}
+	if((document.getElementById("jobTitle").value!="")){
+		if((/^[a-z]+$/i).test(document.getElementById("jobTitle").value)){
+			formData.jobTitleName=document.getElementById("jobTitle").value;
+			if((document.getElementById("jobTitle").nextSibling.className=="error")){
+				document.getElementsByClassName("error")[0].remove();
+			}
+		}else{
+			var parent = document.getElementById("jobTitle");
+			if((document.getElementById("jobTitle").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the Alphabets only</span>");
+				document.getElementById("jobTitle").focus();
+			}
+		}
+	}else{
+			var parent = document.getElementById("jobTitle");
+			if((document.getElementById("jobTitle").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the job title</span>");
+				document.getElementById("jobTitle").focus();
+			}
+		}
+	if((document.getElementById("firstName").value!="")){
+		if((/^[a-z]+$/i).test(document.getElementById("firstName").value)){
+			formData.firstName=document.getElementById("firstName").value;
+			if((document.getElementById("firstName").nextSibling.className=="error")){
+				document.getElementsByClassName("error")[0].remove();
+			}
+		}else{
+			var parent = document.getElementById("firstName");
+			if((document.getElementById("firstName").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the Alphabets only</span>");
+				document.getElementById("firstName").focus();
+			}
+		}
+	}else{
+			var parent = document.getElementById("firstName");
+			if((document.getElementById("firstName").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the first name</span>");
+				document.getElementById("firstName").focus();
+			}
+		}
+	if((document.getElementById("lastName").value!="")){
+		if((/^[a-z]+$/i).test(document.getElementById("lastName").value)){
+			formData.lastName=document.getElementById("lastName").value;
+			if((document.getElementById("lastName").nextSibling.className=="error")){
+				document.getElementsByClassName("error")[0].remove();
+			}
+		}else{
+			var parent = document.getElementById("lastName");
+			if((document.getElementById("lastName").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the Alphabets only</span>");
+				document.getElementById("lastName").focus();
+			}
+		}
+	}else{
+			var parent = document.getElementById("lastName");
+			if((document.getElementById("lastName").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the last name</span>");
+				document.getElementById("lastName").focus();
+			}
+		}
+	if((document.getElementById("lastName").value!="") && (document.getElementById("firstName").value!="")){
+		if((/^[a-z]+$/i).test(document.getElementById("firstName").value) && (/^[a-z]+$/i).test(document.getElementById("lastName").value)){
+			formData.preferredFullName = document.getElementById("firstName").value + " "+
+			 document.getElementById("lastName").value;
+		}
+	}
+	if((document.getElementById("employeeCode").value!="")){
+		if((/^[a-z]*\d+$/i).test(document.getElementById("employeeCode").value)){
+			formData.employeeCode=document.getElementById("employeeCode").value;
+			if((document.getElementById("employeeCode").nextSibling.className=="error")){
+				document.getElementsByClassName("error")[0].remove();
+			}
+		}else{
+			var parent = document.getElementById("employeeCode");
+			if((document.getElementById("employeeCode").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the alphabets with number only. Example abc12</span>");
+				document.getElementById("employeeCode").focus();
+			}
+		}
+	}else{
+			var parent = document.getElementById("employeeCode");
+			if((document.getElementById("employeeCode").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the employee code</span>");
+				document.getElementById("employeeCode").focus();
+			}
+		}
+
+	if((document.getElementById("region").value!="")){
+		if((/^[a-z]+$/i).test(document.getElementById("region").value)){
+			formData.region=document.getElementById("region").value;
+			if((document.getElementById("region").nextSibling.className=="error")){
+				document.getElementsByClassName("error")[0].remove();
+			}
+		}else{
+			var parent = document.getElementById("region");
+			if((document.getElementById("region").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the Alphabets only</span>");
+				document.getElementById("region").focus();
+			}
+		}
+	}else{
+			var parent = document.getElementById("region");
+			if((document.getElementById("region").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the region</span>");
+				document.getElementById("region").focus();
+			}
+		}
+	if((document.getElementById("mobileNumber").value!="")){
+		if((/^\d+$/i).test(document.getElementById("mobileNumber").value) && (document.getElementById("mobileNumber").value.length == 10)){
+			formData.phoneNumber=document.getElementById("mobileNumber").value;
+			if((document.getElementById("mobileNumber").nextSibling.className=="error")){
+				document.getElementsByClassName("error")[0].remove();
+			}
+		}else if(!(isNaN(document.getElementById("mobileNumber").value))){
+			var parent = document.getElementById("mobileNumber");
+			if((document.getElementById("mobileNumber").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Mobile number must be of 10 digit only</span>");
+				document.getElementById("mobileNumber").focus();
+			}else{
+				document.getElementById("mobileNumber").nextElementSibling.innerHTML= "Mobile number must be of 10 digit only";
+			}
+		}else{
+			var parent = document.getElementById("mobileNumber");
+			if((document.getElementById("mobileNumber").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Mobile number must be a number</span>");
+				document.getElementById("mobileNumber").focus();
+			}else{
+				document.getElementById("mobileNumber").nextElementSibling.innerHTML= "Mobile number must be a number";
+			}
+		}
+	}else{
+			var parent = document.getElementById("mobileNumber");
+			if((document.getElementById("mobileNumber").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the mobile number</span>");
+				document.getElementById("mobileNumber").focus();
+			}
+		}
+	if((document.getElementById("emailId").value!="")){
+		if((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i).test(document.getElementById("emailId").value)){
+			formData.emailAddress=document.getElementById("emailId").value;
+			if((document.getElementById("emailId").nextSibling.className=="error")){
+				document.getElementsByClassName("error")[0].remove();
+			}
+		}else{
+			var parent = document.getElementById("emailId");
+			if((document.getElementById("emailId").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the proper email id format</span>");
+				document.getElementById("emailId").focus();
+			}
+		}
+	}else{
+			var parent = document.getElementById("emailId");
+			if((document.getElementById("emailId").nextSibling.className==null)){
+				parent.insertAdjacentHTML("afterend", "<span class='error'>Please enter the email id</span>");
+				document.getElementById("emailId").focus();
+			}
+		}
+		if(document.getElementsByClassName("error")[0]==null){
+			if(pageType == "createEmployee"){
+				sendData(formData);
+			}else if(pageType == "editEmployeePage"){
+				editEmployee(formData);
+			}
+			
+	}else{
+		return false;
+	}
+}
+
+
+
+
+
+
